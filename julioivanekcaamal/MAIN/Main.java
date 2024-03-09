dc5a6bcdc18fe6acebc9f5ffc4141f7eee8b8620
Julioek8866 <julioivanekcaamal3432@gmail.com>
2024-03-09 11:52:58 -0600

    add: Styles

--- julioivanekcaamal/MAIN/Main.java
+++ julioivanekcaamal/MAIN/Main.java
@@ -1,12 +1,6 @@
 package julioivanekcaamal.MAIN;
 
-import julioivanekcaamal.ANIMAL.Perro;
-import julioivanekcaamal.AVATAR.Avatar;
-import julioivanekcaamal.HOLAMUNDO.MiHolaMundo;
-import julioivanekcaamal.NOTA.Nota;
-import julioivanekcaamal.OBJETO.Calculadora;
 import julioivanekcaamal.PERRO1.Perro1;
-import julioivanekcaamal.TERMOMETRO_DIGITAL.TermometroDigital;
 
 /**
  * Main
