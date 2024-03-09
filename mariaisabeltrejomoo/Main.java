f00c94bbe6ee0714fb7f1476d84274599147ad29
Isabel-Trejo <trejoisabel592@gmail.com>
2024-03-09 09:31:21 -0600

    cambio

--- mariaisabeltrejomoo/Main.java
+++ mariaisabeltrejomoo/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Computadora c=new Computadora("LENOVO", "verde", "ARM23", 34, "Windows");
         c.encender();
-        System.out.println(c.toString());
+        c.info();
         c.reiniciar();
         c.apagar();
     }
