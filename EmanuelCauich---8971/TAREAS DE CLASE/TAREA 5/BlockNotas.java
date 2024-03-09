28756d00e00640a19a8c8325ba3ff29d5c4e4509
EmanuelCauichCanul <emanuelcauich048@gmail.com>
2024-03-09 12:31:19 -0600

    block de notas

--- EmanuelCauich---8971/TAREAS DE CLASE/TAREA 5/BlockNotas.java
+++ EmanuelCauich---8971/TAREAS DE CLASE/TAREA 5/BlockNotas.java
@@ -1,24 +1,55 @@
+import java.util.Vector;
+
 public class BlockNotas {
-    private int pagina;
-    private String titulo;
+    private Vector <Nota> notas;
+    private int numNota;
 
-    public BlockNotas (int pagina, String titulo){
-
+    public BlockNotas(){
+        numNota = 0;
+        notas = new Vector<Nota>();
     }
-
-    public  int getPagina(){
-        return pagina;
+    public boolean addNota(Nota nota){
+        notas.add(nota);
+        numNota++;
+        return true;
     }
-    public String getTitulo(){
-        return titulo;
+    public Nota getNota(int num) {
+        if (!notas.isEmpty() && numNota > num) {
+            return notas.get(num);
+        }
+        return new Nota("", "", "", "", "");
+    }    
+    public boolean deleteNota(int num){
+        if (!notas.isEmpty()&& numNota > num) {
+            notas.remove(num);
+            numNota--;
+            return true;
+        }
+        return false;
     }
-    public void setPagina(int pagina){
-        this.pagina = pagina;
+    public boolean updateNota(int num, Nota n){
+        if (!notas.isEmpty()&& numNota > num) {
+            notas.set(num, n);
+            return true;
+        }
+        return false;
     }
-    public void setTitulo(String titulo){
-        this.titulo = titulo;
-    }
-    public void imprimir(){
-        
+    public boolean updateNota(int num, String tipo, String texto, String autor, String fechaLimite){
+        if (!notas.isEmpty()&& numNota>num) {
+            if (texto.equals("")) {
+                notas.get(num).setTexto(texto);
+            }
+            if (texto.equals("")) {
+                notas.get(num).setAutor(autor);
+            }
+            if (texto.equals("")) {
+                notas.get(num).setTipo(tipo);
+            }
+            if (texto.equals("")) {
+                notas.get(num).setFechaLimite(fechaLimite);
+            }
+            return true;
+        }
+        return false;
     }
 }
