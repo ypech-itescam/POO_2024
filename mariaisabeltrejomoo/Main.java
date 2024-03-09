b7bd701c0d061c74c322af05fa657a7a9cc9d9bc
Isabel-Trejo <trejoisabel592@gmail.com>
2024-03-09 16:38:29 -0600

    cambio en computadora

--- mariaisabeltrejomoo/Main.java
+++ mariaisabeltrejomoo/Main.java
@@ -1,6 +1,4 @@
 public class Main {
-    public Computadora c;
-
     public static void main(String[] args) {
         Computadora c=new Computadora("LENOVO", "verde", "ARM23", 34, "Windows");
         c.encender();
