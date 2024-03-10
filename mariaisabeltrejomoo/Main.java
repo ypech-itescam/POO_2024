c79110571321e42883fa83997b45f4154b3ec426
Isabel-Trejo <trejoisabel592@gmail.com>
2024-03-09 18:45:39 -0600

    cambio

--- mariaisabeltrejomoo/Main.java
+++ mariaisabeltrejomoo/Main.java
@@ -1,12 +1,14 @@
+package mariaisabeltrejomoo;
+
 public class Main {
     public static void main(String[] args) {
         Computadora c = new Computadora("HP", "Negro","", 16, "Linux");
+        c.encender();
         System.out.println("Marca: " + c.getMarca());
         System.out.println("Color: " + c.getColor());
         System.out.println("Modelo: " + c.getModelo());
         System.out.println("Memoria RAM: " + c.getMemoriaRAM() + "GB");
         System.out.println("Tipo de Sistema: " + c.getTipoSistema());
-        c.encender();
         c.reiniciar();
         c.apagar();
     }
