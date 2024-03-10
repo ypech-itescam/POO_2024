d1f958d44e3a0f9df90b73095c63c25307adaff0
Isabel-Trejo <trejoisabel592@gmail.com>
2024-03-10 00:09:41 -0600

    cambio

--- mariaisabeltrejomoo/Computadora.java
+++ mariaisabeltrejomoo/Computadora.java
@@ -51,4 +51,15 @@ public class Computadora {
     public void apagar() {
         System.out.println("Apagando...");
     }
+    public static void main(String[] args) {
+        Computadora c = new Computadora("HP", "Negro","", 16, "Linux");
+        c.encender();
+        System.out.println("Marca: " + c.getMarca());
+        System.out.println("Color: " + c.getColor());
+        System.out.println("Modelo: " + c.getModelo());
+        System.out.println("Memoria RAM: " + c.getMemoriaRAM() + "GB");
+        System.out.println("Tipo de Sistema: " + c.getTipoSistema());
+        c.rena();
+        c.apagar();
+    }
 }
