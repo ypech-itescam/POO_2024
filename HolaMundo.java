5d9d580a9a79421ac9fd5574140d97db42d7541a
JOSUE HILARIO CAB KU <160068705+8978josue@users.noreply.github.com>
2024-03-14 16:58:12 -0600

    Add files via upload

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,48 +1,32 @@
-public class HolaMundo{
+public class HolaMundo {
 
-    private String texto;
-    private String color;
-
-//constructor :v
-
-    public HolaMundo(String texto, String color) {
-        this.texto = texto.equals("")?"Hola Mundo":texto;
-        this.color = color;
+    public static void main(String[] args) {
+      
+        imprimirEnColor("Hola Mundo", Color.ROJO);
+        imprimirEnColor("Hello World", Color.AZUL);
+        imprimirEnColor("Bonjour le monde", Color.VERDE);
     }
 
-//metodos de acceso
-    public void setTexto(String texto){
-        this.texto=texto.equals("")?"Hola Mundo":texto;
+    public static void imprimirEnColor(String mensaje, Color color) {
+        String codigoColor = color.getCodigo();
+        String colorPredeterminado = "\u001B[0m";
+
+        System.out.println(codigoColor + mensaje + colorPredeterminado);
+    }
+}
+
+enum Color {
+    ROJO("\u001B[31m"),
+    AZUL("\u001B[34m"),
+    VERDE("\u001B[32m");
+
+    private final String codigo;
+
+    Color(String codigo) {
+        this.codigo = codigo;
     }
 
-    public String getTexto(){
-        return texto;
-    }
-    
-    public void setColor(String color){
-        switch (color = color.toLowerCase()) {
-            case "negro":this.color="\033[30m";break;
-            case "rojo":this.color="\033[31m"; break;
-            case "verde":this.color="\033[32m";break;          
-            case "amarillo":this.color="\033[33m";break;
-            case "azul":this.color="\033[34m"; break;
-            case "blanco":this.color="\033[37m";break;
-            default: this.color="\u001B[0m";break;
-        }
-    }
-
-    public String getColor(){
-        return color;
-    }
-
-//metodos de accion
-    public void Mayusculas(){
-        this.texto=this.texto.toUpperCase();
-    }
-    public void Minusculas(){
-        this.texto = this.texto.toLowerCase();
-    }
-    public String mostrar(){
-        return getColor()+getTexto()+"\u001B[0m";
+    public String getCodigo() {
+        return codigo;
     }
 }
