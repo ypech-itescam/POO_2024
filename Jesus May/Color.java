78f8398da33b2ebe76234f8bf445c45b082d95cf
Jesus May <jesusbrito777@gmail.com>
2024-03-15 08:49:48 -0600

    Jesus

--- Jesus May/Color.java
+++ Jesus May/Color.java
@@ -2,39 +2,40 @@ public class Color{
     private String color;
     private String colorAnsi;
 
-     public Color(String Color){
+     public Color(String color){
         setColor(color);
      }
 
      public void setColor(String color) {
-         this.color = color;
-         switch (color.toLowerCase()) {
+         switch (color) {
             case "negro":
-                this.colorAnsi = "\033[30m";
+            this.color = color;
+                colorAnsi = "\033[30m";
                 break;
             case "rojo":
-                this.colorAnsi = "\033[31m";
+            this.color = color;
+                colorAnsi = "\033[31m";
                 break;
             case "verde":
-                this.colorAnsi = "\033[32m";
+            this.color = color;
+                colorAnsi = "\033[32m";
                 break;
             case "amarillo":
-                this.colorAnsi = "\033[33m";
+            this.color = color;
+                colorAnsi = "\033[33m";
                 break;
             case "azul":
-                this.colorAnsi = "\033[34m";
+            this.color = color;
+                colorAnsi = "\033[34m";
                 break;
             default:
-                this.colorAnsi = "\033[37m";
+            this.color = color;
+                colorAnsi = "\033[37m";
         };
      }
 
      public String getColor() {
          return color;
-     }
-
-     public void setColorAnsi(String colorAnsi) {
-         this.colorAnsi = colorAnsi;
      }
 
      public String getColorAnsi() {
--- Jesus May/Nota.java
+++ Jesus May/Nota.java
@@ -4,15 +4,15 @@ public class Nota {
     private String texto;
     private String fechaLimite;
     private String nombre;
-    private String colorHex;
+    private Color color;
 
-    public Nota(String importancia, String fecha, String texto, String fechaLimite, String nombre, String color) {
+    public Nota(String importancia, String fecha, String fechaLimite, String texto) {
         setImportancia(importancia);
         setFecha(fecha);
         setTexto(texto);
         setFechaLimite(fechaLimite);
-        setNombre(nombre);
-        setColor(color);
+        setNombre("Jesús");
+        color = new Color("verde");
     }
 
     public String getImportancia() {
@@ -20,7 +20,7 @@ public class Nota {
     }
 
     public void setImportancia(String importancia) {
-        this.importancia = importancia.trim().isEmpty() ? "urgente" : importancia;
+        this.importancia = importancia.equals("") ? "urgente" : importancia;
     }
 
     public String getFecha() {
@@ -28,7 +28,7 @@ public class Nota {
     }
 
     public void setFecha(String fecha) {
-        this.fecha = fecha.trim().isEmpty() ? "19 de febrero" : fecha;
+        this.fecha = fecha.equals("") ? "19 de febrero" : fecha;
     }
 
     public String getTexto() {
@@ -36,7 +36,7 @@ public class Nota {
     }
 
     public void setTexto(String texto) {
-        this.texto = texto.trim().isEmpty() ? "Realiza la practica" : texto;
+        this.texto = texto.equals("")? "Realiza la practica" : texto;
     }
 
     public String getFechaLimite() {
@@ -44,7 +44,7 @@ public class Nota {
     }
 
     public void setFechaLimite(String fechaLimite) {
-        this.fechaLimite = fechaLimite.trim().isEmpty() ? "1 de febrero" : fechaLimite;
+        this.fechaLimite = fechaLimite.equals("")? "1 de febrero" : fechaLimite;
     }
 
     public String getNombre() {
@@ -52,63 +52,18 @@ public class Nota {
     }
 
     public void setNombre(String nombre) {
-        this.nombre = nombre.trim().isEmpty() ? "Yaqui" : nombre;
+        this.nombre = nombre.equals("") ? "Yaqui" : nombre;
     }
 
-    public String getColorHex() {
-        return colorHex;
+    public Color getColor() {
+        return color;
     }
 
     public void setColor(String color) {
-        switch (color.toLowerCase()) {
-            case "negro":
-                this.colorHex = "\033[30m";
-                break;
-            case "rojo":
-                this.colorHex = "\033[31m";
-                break;
-            case "verde":
-                this.colorHex = "\033[32m";
-                break;
-            case "amarillo":
-                this.colorHex = "\033[33m";
-                break;
-            case "azul":
-                this.colorHex = "\033[34m";
-                break;
-            default:
-                this.colorHex = "\033[37m";
-        }
+        this.color.setColor(color);;
     }
 
-    public String show() {
-        return (
-            getColorHex() + "Importancia: " + getImportancia() + "\n" +
-            "Fecha: " + getFecha() + "\n" +
-            "Texto: " + getTexto() + "\n" +
-            "Fecha Límite: " + getFechaLimite() + "\n" +
-            "Nombre: " + getNombre() + "\n" +
-            "\u001B[0m"
-        );
-    }
-
-    public static void main(String[] args) {
-       
-        System.out.print("Importancia: ");
-       
-        System.out.print("Fecha: ");
-
-        System.out.print("Texto: ");
-        
-        System.out.print("Fecha Límite: ");
-
-        System.out.print("Nombre: ");
-       
-        System.out.print("Color (negro, rojo, verde, amarillo, azul, por defecto): ");
-       
-        Nota nota = new Nota ("importancia", "fecha", "texto", "fechaLimite", "nombre","color");
-        System.out.println(Nota());
-
-        
+    public String toString() {
+        return (color.getColorAnsi() + "Importancia: " + getImportancia() + "\n" + "Fecha: " + getFecha() + "\n" + "Texto: " + getTexto() + "\n" + "Fecha Límite: " + getFechaLimite() + "\n" + "Nombre: " + getNombre() + "\n" + "\u001B[0m");
     }
 }
